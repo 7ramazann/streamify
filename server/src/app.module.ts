d4fb8cb73ca24a2c31e6c4f33a0ece593feff7ec
Ramazan <user@Users-MacBook-Air.local>
2025-04-30 12:15:13 +0600

    w3 runs

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoreModule } from './core/core.module';
+import { AccountModule } from './modules/auth/account/account.module';
 
 @Module({
-  imports: [CoreModule],
+  imports: [CoreModule, AccountModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/core/core.module.ts
+++ server/src/core/core.module.ts
@@ -5,6 +5,7 @@ import { ApolloDriver } from '@nestjs/apollo';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { getGraphQLConfig } from './config/graphql.config';
 import { RedisModule } from './redis/redis.module';
+import { AccountModule } from 'src/modules/auth/account/account.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { RedisModule } from './redis/redis.module';
     }),
     PrismaModule,
     RedisModule,
+    AccountModule
   ],
 })
 export class CoreModule {}
