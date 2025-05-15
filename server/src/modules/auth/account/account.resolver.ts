c4a1909964d540293355542d10633dc6f150e451
Ramazan <user@Users-MacBook-Air.local>
2025-05-15 11:58:15 +0600

    W4 User Registration with GraphQL Mutation & DTO Validation

--- server/src/modules/auth/account/account.resolver.ts
+++ server/src/modules/auth/account/account.resolver.ts
@@ -1,6 +1,7 @@
-import { Query, Resolver } from '@nestjs/graphql'
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
 import { AccountService } from './account.service'
 import { UserModel } from './models/user.model'
+import { CreateUserInput } from './inputs/create-user.input'
 
 @Resolver('Account')
 export class AccountResolver {
@@ -9,5 +10,11 @@ export class AccountResolver {
   @Query(() => [UserModel], { name: 'findAllUsers' })
   public async findAll() {
     return this.accountService.findAll()
+  }
+
+  @Mutation(() => Boolean, { name: 'createUser' })
+  public async create(@Args('data') input: CreateUserInput): Promise<boolean> {
+    // Call accountService.create with input and return result
+    return this.accountService.create(input);
   }
 }
--- server/src/modules/auth/account/account.service.ts
+++ server/src/modules/auth/account/account.service.ts
@@ -1,8 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { CreateUserInput } from './inputs/create-user.input';
+import { PrismaService } from '@/core/prisma/prisma.service';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AccountService {
-    findAll() {
-        return [];
+    constructor(private readonly prisma: PrismaService) {}
+
+  
+    async findAll() {
+      return this.prisma.user.findMany();
+    }    
+
+    public async create(input: CreateUserInput) {
+      // Destructure username, email, password from input
+      const { username, email, password } = input;
+
+      // Check if username or email already exists in the database
+      const existingUser = await this.prisma.user.findFirst({
+        where: {
+          OR: [{ username }, { email }],
+        },
+      });
+  
+      // If yes, throw ConflictException
+      if (existingUser) {
+        throw new ConflictException('Username or email already exists');
       }
+      
+      // Create new user with:
+      // - hashed password, email, default displayName from username
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      await this.prisma.user.create({
+        data: {
+          username,
+          email,
+          password: hashedPassword,
+          displayName: username
+        }
+      })
+
+      return true
+    }
 }
